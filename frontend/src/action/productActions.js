facf1717bbe9ede6044f27afd47ca31cc3c48f4e
Aamishsn <aamishsn@gmail.com>
2024-07-20 17:26:52 +0500

    redux consants added

--- frontend/src/action/productActions.js
+++ frontend/src/action/productActions.js
@@ -1,12 +1,18 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
 import axios from "axios";
 export const listProductsAction = async (dispatch) => {
   try {
-    dispatch({ type: "PRODUCT_LIST_REQUEST" });
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
     const { data } = await axios.get("http://127.0.0.1:8000/api/products/");
 
-    dispatch({ type: "PRODUCT_LIST_SUCCESS", payload: data });
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (err) {
-    dispatch({ type: "PRODUCT_LIST_FAIL", payload: err.message });
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message });
   }
 };
