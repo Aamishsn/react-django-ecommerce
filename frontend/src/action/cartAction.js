a206999e280ad439984704a7d3fc9d7db0da3b5c
Aamishsn <aamishsn@gmail.com>
2024-07-22 21:03:42 +0500

    cart redux action for add to cart added

---
+++ frontend/src/action/cartAction.js
@@ -0,0 +1,18 @@
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`http://127.0.0.1:8000/api/products/${id}`);
+
+  dispatch({
+    type: "CART_ADD_ITEM",
+    payload: {
+      product_id: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity: qty,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+
+};
